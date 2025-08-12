efd7219e48538dc074cf15860edbb8f7ceceac49
Lenox <lenoxviews@gmail.com>
2025-08-12 10:28:27 +0300

    worked on error handling GLFW functions

--- todo.c
+++ todo.c
@@ -1,7 +1,20 @@
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 int main () {
     glfwInit();
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return -1;
+    }
+    
     GLFWwindow* window = glfwCreateWindow(900, 600, "Lens View", NULL, NULL);
+    if (!window)
+    {
+        fprintf(stderr, "Failed to create window\n");
+        glfwTerminate();
+        return -1;
+    }
+    
     glfwMakeContextCurrent(window);
     while (!glfwWindowShouldClose(window))
     {
